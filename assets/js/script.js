dfc7ba222c674c3a97433ac241cd94b07151916a
Tommy Boileau <tommyboileau@Tommys-MacBook-Air.local>
2022-08-11 13:29:35 -0700

    refactored some of the code and added a high score button on the start page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,21 @@
 //variable declarations
-var startButton = $(".start-button");
+var startButton = $("#start-button");
+var hsButton = $("#hs-button");
 var score = $(".score");
 var timer = $(".timer");
 var paraBox = $(".paraBox");
-var highScore = $(".initialForm");
-var userInitials = $("#initials");
-var answerButtons = $("#qaButtons");
-var questionBox = $(".card-question");
-var scoreNames = $(".scoreNames");
+var scoreArea = $("#scoreArea");
+// var answerButtons = $("#qaButtons");
+var questionsBox = $("#questionsBox");
 var points = 0;
+var secondsLeft = 30;
+let qNum;
 
 //hide elements that wont be used til later on the initial page load
 score.hide();
 timer.hide();
-highScore.hide();
-
-var secondsLeft = 30;
+scoreArea.hide();
+questionsBox.hide();
 
 //set timer interval in variable
 function startTimer() {
@@ -36,65 +36,70 @@ function startTimer() {
 //create arrays for each of the questions to be asked in the quiz along with their answers
 
 const questions = [
-    {
-        question: "How do you know if a function is being called?",
-        answers: [
-            { text: "The command '.call' is after the function", correct: false},
-  {text: "You do not need to call a function", correct: false},
-  { text: "The function is folowed by '()'", correct: true},
-  {text: "You yell at the computer to 'DO SOMETHING!!'", correct: false},
-        ]
+  {
+    question: "How do you know if a function is being called?",
+    answers: [
+      { text: "The command '.call' is after the function", correct: false },
+      { text: "You do not need to call a function", correct: false },
+      { text: "The function is folowed by '()'", correct: true },
+      { text: "You yell at the computer to 'DO SOMETHING!!'", correct: false },
+    ],
+  },
+  {
+    question: "What can you do to make an element do something when clicked?",
+    answers: [
+      { text: "All elements are automatically resonsive.", correct: false },
+      { text: "You must add an event listener to the element", correct: true },
+      { text: "Double click on the element.", correct: false },
+      { text: "Set an interval to the element", correct: false },
+    ],
+  },
 
+  {
+    question: "What does the '.push' operator do?",
+    answers: [
+      { text: "It pushes the code to the web page.", correct: false },
+      { text: "It adds indentation to your web page", correct: false },
+      {
+        text: "It reminds you to get up and do some push-ups!",
+        correct: false,
+      },
+      { text: "It adds an item to the end of an array.", correct: true },
+    ],
+  },
 
-    },
-    {question: "What can you do to make an element do something when clicked?",
-        answers: [
-            { text: "All elements are automatically resonsive.", correct: false},
-  {text: "You must add an event listener to the element", correct: true},
-  { text: "Double click on the element.", correct: false},
-  {text: "Set an interval to the element", correct: false}, ] },
+  {
+    question: "What is the strict equality comparison operator?",
+    answers: [
+      { text: "=", correct: false },
+      { text: "==", correct: false },
+      { text: "!=", correct: false },
+      { text: "===", correct: true },
+    ],
+  },
+];
 
-  {question: "What does the '.push' operator do?",
-        answers: [
-            { text: "It pushes the code to the web page.", correct: false},
-  {text: "It adds indentation to your web page", correct: false},
-  { text: "It reminds you to get up and do some push-ups!", correct: false},
-  {text: "It adds an item to the end of an array.", correct: true}, ] }
+// function renderQuestionAnswers() {
+//   // Dynamically create buttons for the answers.
+//   for (var i = 0; i < answers1.length; i++) {
+//     // Create button
+//     var answerBtn = $("<button>");
+//     // Assign answers to the buttons
+//     answerBtn.attr("data-answer", answers1[i]);
+//     // Display the answers
+//     answerBtn.text(answers1[i]);
+//     // Attach the buttons
+//     answerButtons.append(answerBtn);
+//   }
 
-
-  {question: "What is the strict equality comparison operator?",
-        answers: [
-            { text: "=", correct: false},
-  {text: "==", correct: false},
-  { text: "!=", correct: false},
-  {text: "===", correct: true}, ] }
-]
-
-function renderQuestionAnswers() {
-  // Dynamically create buttons for the answers.
-  for (var i = 0; i < answers1.length; i++) {
-    // Create button
-    var answerBtn = $("<button>");
-    // Assign answers to the buttons
-    answerBtn.attr("data-answer", answers1[i]);
-    // Display the answers
-    answerBtn.text(answers1[i]);
-    // Attach the buttons
-    answerButtons.append(answerBtn);
-  }
-
-  //Attach the Question
-  var questionh3 = $("<h3>");
-  // Assign question to the h3
-  questionh3.attr("data-question", question1);
-  // Display the question
-  questionh3.text(question1);
-  // Attach the h3
-  questionBox.append(questionh3);
-}
-
-// if (answerBtn.on("click" === correctAnswers)) {
-//   score + 10;
+//   //Attach the Question
+//   var questionh3 = $("<h3>");
+//   // Assign question to the h3
+//   questionh3.attr("data-question", question1);
+//   // Display the question
+//   questionh3.text(question1);
+//   // Attach the h3
+//   questionBox.append(questionh3);
 // }
 
 //place the scores into local storage
@@ -119,18 +124,19 @@ function renderScores(event) {
 }
 
 function endGame() {
-  highScore.show();
+  scoreArea.show();
   renderScores();
 }
-//handles the scoreboard when the user initials is submitted
-userInitials.on("submit", renderScores);
 
 //start the game on the slick of start button
 startButton.on("click", function () {
   startButton.hide();
+  hsButton.hide();
   paraBox.hide();
   score.show();
   timer.show();
+  questionsBox.show();
+  qNum = 0;
   startTimer();
-  renderQuestionAnswers();
+  //   renderQuestionAnswers();
 });
